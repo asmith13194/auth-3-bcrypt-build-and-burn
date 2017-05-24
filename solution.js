089f9986cb610a8f5cdd1b8547e56a9d0514924b
Adam Smith <asmith13194@gmail.com>
2017-05-24 14:00:39 -0600

    jodie is awesome

--- solution.js
+++ solution.js
@@ -1,9 +1,12 @@
+var bcrypt = require('bcrypt')
+// console.log(bcrypt);
 function hash(plaintext) {
-
+  console.log(plaintext);
+  return bcrypt.hashSync(plaintext,8)
 }
 
 function compare(plaintext, hash) {
-
+  return bcrypt.compareSync(plaintext,hash)
 }
 
 module.exports = {
